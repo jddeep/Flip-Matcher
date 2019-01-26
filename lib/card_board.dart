6c20d1be228d901e27e61503c157c089a3a9169c
jd003walker <41433222+jd003walker@users.noreply.github.com>
2019-01-26 18:43:14 +0530

    First commit

---
+++ lib/card_board.dart
@@ -0,0 +1,162 @@
+import 'dart:async';
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'main.dart';
+import 'card_item.dart';
+
+class CardBoard extends StatefulWidget {
+  final Function() onWin;
+  final BuildContext context;
+   
+  //const CardBoard({Key key, this.onWin}) : super(key: key);
+  CardBoard({Key key, this.onWin, this.context}) : super(key:key);
+  @override
+  State<StatefulWidget> createState() {
+    return CardBoardState();
+  }
+}
+
+class CardBoardState extends State<CardBoard> {
+  List<int> openedCards = [];
+  List<CardModel> cards;
+  int a;
+  int score = 0;
+  int time = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    a=1;
+    cards = createCards();
+  }
+
+
+  List<CardModel> createCards() {
+    List<String> asset = [];
+    List(10).forEach((f) => asset.add('0${(asset.length + 1)}.png'));
+    List(10).forEach((f) => asset.add('0${(asset.length - 10 + 1)}.png'));
+    return List(20).map((f) {
+      int index = Random().nextInt(1000) % asset.length;
+      String _image =
+          'assets/' + asset[index].substring(asset[index].length - 6);
+      asset.removeAt(index);
+      return CardModel(
+          id: 20 - asset.length - 1, image: _image, key: UniqueKey());
+    }).toList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(
+        padding: EdgeInsets.zero,
+        //  physics: NeverScrollableScrollPhysics(),
+        shrinkWrap: true,
+        crossAxisCount: 4,
+        childAspectRatio: 322 / 400,
+        children: cards
+            .map((f) =>
+            CardItem(key: f.key, model: f, onFlipCard: handleFlipCard))
+            .toList());
+  }
+
+  void handleFlipCard(bool isOpened, int id) {
+    cards[id].isNeedCloseEffect = false;
+
+    checkOpenedCard(isOpened);
+
+    if (isOpened) {
+      setCardOpened(id);
+      openedCards.add(id);
+    } else {
+      setCardNone(id);
+      openedCards.remove(id);
+    }
+
+    checkWin();
+    checkOver();
+  }
+
+  void checkOver(){
+
+    if(a >= 11){
+      a=1;
+      BuildContext context = widget.context;
+      showDialog(
+        context: context,
+        builder: (context){
+          return AlertDialog(
+            title: Text("Congratulations!"),
+            content: Text("You WIN !"),
+            actions: <Widget>[
+              FlatButton(
+                onPressed: (){
+                  cards = createCards();
+                  Navigator.pop(context, MaterialPageRoute(builder: (context) => MyHomePage(score: 0, time: 0)));
+                  
+                },
+                child: Text("Play Again"),
+              ),
+            ],
+          );
+        }
+      );
+      
+
+      
+    }
+  }
+
+  // void resetTimer(){
+  //   Timer(Duration(seconds: 1), (){
+  //     setState(() {
+  //             this.time+=1;
+  //             resetTimer();
+  //           });
+  //   });
+  // }
+
+  void checkOpenedCard(bool isOpened) {
+    if (openedCards.length == 2 && isOpened) {
+      cards[openedCards[0]].isNeedCloseEffect = true;
+      setCardNone(openedCards[0]);
+      cards[openedCards[1]].isNeedCloseEffect = true;
+      setCardNone(openedCards[1]);
+      openedCards.clear();
+    }
+  }
+
+  void checkWin() {
+    if (openedCards.length == 2) {
+      if (cards[openedCards[0]].image == cards[openedCards[1]].image) {
+        setCardWin(openedCards[0]);
+        setCardWin(openedCards[1]);
+        openedCards.clear();
+        a++;
+        widget.onWin();
+      }
+    }
+  }
+
+  void setCardNone(int id) {
+    setState(() {
+      cards[id].status = ECardStatus.None;
+      cards[id].key = UniqueKey();
+    });
+  }
+
+  void setCardOpened(int id) {
+    setState(() {
+      cards[id].status = ECardStatus.Opened;
+      cards[id].key = UniqueKey();
+    });
+  }
+
+  void setCardWin(int id) {
+    setState(() {
+      cards[id].status = ECardStatus.Win;
+      cards[id].key = UniqueKey();
+      
+    });
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,249 @@
+import 'dart:async';
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'card_board.dart';
+import 'card_item.dart';
+
+void main() {
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      debugShowMaterialGrid: false,
+      home: new MyHomePage(),
+    );
+  }
+}
+
+class RestartWidget extends StatefulWidget {
+  final Widget child;
+  RestartWidget({this.child});
+
+  static restartApp(BuildContext context){
+    final _RestartWidgetState state = context.ancestorStateOfType(const TypeMatcher<_RestartWidgetState>());
+    state.restartApp();
+  }
+  @override
+  _RestartWidgetState createState() => _RestartWidgetState();
+}
+
+class _RestartWidgetState extends State<RestartWidget> {
+  Key key = UniqueKey();
+
+  void restartApp(){
+    this.setState((){
+      key = UniqueKey();
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      key: key,
+      child: widget.child,
+    );
+  }
+}
+
+
+class MyHomePage extends StatefulWidget {
+  final score;
+  final time;
+    MyHomePage({Key key, this.score, this.time}): super(key:key);
+  MyHomePageState createState() => new MyHomePageState();
+}
+
+class MyHomePageState extends State<MyHomePage> {
+  int score = 0;
+  int time = 0;
+  int init = 0;
+
+
+  @override
+  void initState() {
+    super.initState();
+    init = 1;
+    // Timer(Duration(seconds: 2), runTimer);
+
+    
+  }
+
+
+  void showalert(BuildContext context){
+   showDialog(
+     context: context,
+     builder: (context){ 
+      return AlertDialog(
+          title: Text('Welcome to Card Flipper',
+        style: TextStyle(
+          fontSize: 28.0,
+          fontFamily: 'GoogleSans',
+          color: Colors.black,
+        ),
+        ),
+        content: Text('Match two cards to score points. 1 pair match = 200 points',
+         style: TextStyle(
+           fontSize: 22.0,
+           fontFamily: 'GoogleSans',
+           color: Colors.grey,
+         ),
+        ),
+        actions: <Widget>[
+          FlatButton(
+            onPressed: (){
+              Navigator.pop(context);
+              Timer(Duration(seconds: 0), runTimer);
+            },
+            child: Text(" Let's play !",
+           style: TextStyle(
+             fontSize: 20.0,
+             fontFamily: 'GoogleSans',
+             color: Colors.purple, 
+           ),
+          ),
+          )
+        ],
+     );
+     }
+   );
+  }
+  
+  void runTimer() {
+    
+    Timer(Duration(
+      seconds: 1
+      ), () {
+      setState(() {
+        if(this.score == 2000){
+          this.time = -2;
+          this.score = 0;
+        }
+        this.time += 1;
+        runTimer();
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if(init == 1){
+      init++;
+    Future.delayed(Duration.zero, ()=> showalert(context));
+    }
+    return new Scaffold(
+        backgroundColor: Colors.white,
+        body: Container(
+          decoration: BoxDecoration(
+            
+              // gradient: LinearGradient(
+              //     begin: Alignment.topCenter,
+              //     end: Alignment.bottomCenter,
+              //     colors: [
+              //   Colors.black87,
+              //   Colors.black87,
+              //   Colors.orange,
+              //   Colors.black87,
+              //   Colors.black87
+              // ])),
+               
+              ),
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 24.0),
+              initializeGameScore(),
+              initGameBoard(context)
+            ],
+          ),
+        ),
+        );
+  }
+
+
+  Widget initializeGameScore(){
+    return buildScore();
+    
+
+  }
+
+  Widget initGameBoard(BuildContext bcContext){
+    return buildBoard(bcContext);
+  }
+
+  Widget buildScore() {
+    return Padding(
+      padding: EdgeInsets.only(left: 8.0, right: 8.0, top: 7.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Text(time.toString() + "s",
+              style: TextStyle(
+                  fontSize: 32.0,
+                  color: Colors.black,
+                  fontFamily: 'GoogleSans')),
+             
+          Text('Score: '+score.toString(),
+              style: TextStyle(
+                  fontSize: 32.0,
+                  color: Colors.black,
+                  fontFamily: 'GoogleSans'),
+                  )
+        ],
+      ),
+    );
+  }
+
+  Widget buildBoard(BuildContext bcontext) {
+    return Flexible(
+        child: Stack(
+      children: <Widget>[
+        Padding(padding: EdgeInsets.all(10.0), child: CardBoard(onWin: onWin, context: bcontext,)),
+        
+        // buildGradientView()
+      ],
+       
+    ),
+    
+    );
+  }
+
+  Widget buildGradientView() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Container(
+          width: MediaQuery.of(context).size.width,
+          height: 32.0,
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [Colors.black, Colors.black, Colors.transparent])),
+        ),
+        Container(
+          width: MediaQuery.of(context).size.width,
+          height: 32.0,
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [Colors.transparent, Colors.black, Colors.black])),
+        )
+      ],
+    );
+  }
+
+  void onWin() {
+    
+    setState(() { 
+      if(this.score == 2000){
+        this.time = -2;
+      }
+      this.score += 200;}
+    );
+  
+  }
+}
